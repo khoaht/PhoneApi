a94243bd255c71ce12da9d5501bebac1a0d3dc2a
khoaht <hotankhoa.qn@gmail.com>
2014-08-21 22:09:30 +0700

    Release#1 20140821

--- WebPhoneApi/Infrastructure/Domain/MessageRespone.cs
+++ WebPhoneApi/Infrastructure/Domain/MessageRespone.cs
@@ -9,16 +9,7 @@ namespace Infrastructure.Domain
     public class MessageRespone
     {
         public string Status { get; set; }
-        public ErrorInfo ErrorInfo { get; set; }
-        public List<Extension> Extensions { get; set; }
-    }
-
-    public class ErrorInfo
-    {
-        public string MessageInfo{get;set;}
-    }
-
-    public class Extension {
-        public string Data { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
     }
 }
--- WebPhoneApi/WebPhoneApi/Controllers/PhoneController.cs
+++ WebPhoneApi/WebPhoneApi/Controllers/PhoneController.cs
@@ -95,54 +95,51 @@ namespace TeleGoApi.Controllers
         public MessageRespone GetExtension(string userName, string password, int customerId, string callerId)
         {
             MessageRespone respone = new MessageRespone();
-            respone.Extensions = new List<Extension>();
-            respone.ErrorInfo = new Infrastructure.Domain.ErrorInfo();
+
             //check login 
             bool isLogined = userService.ValidateUser(userName, password);
             if (!isLogined)
             {
                 respone.Status = Infrastructure.StatusCode.Failure;
-                respone.ErrorInfo.MessageInfo = "not authorized";
+                respone.Message = "not authorized";
                 return respone;
             }
-            if (!String.IsNullOrEmpty(userName) && !String.IsNullOrEmpty(password) &&  customerId>0 && !String.IsNullOrEmpty(callerId))
+            if (!String.IsNullOrEmpty(userName) && !String.IsNullOrEmpty(password) && customerId > 0 && !String.IsNullOrEmpty(callerId))
             {
                 //TODO:
                 //1. Telego call HHAExchange : callerId, appname,AppKey, AppSercret
                 WebPhoneApi.HhaExchange2.SearchAPISoapClient clientSoap = new WebPhoneApi.HhaExchange2.SearchAPISoapClient();
-                WebPhoneApi.HhaExchange2.CallerIDLookupResponse restResult = clientSoap.GetCallerDataByCallerID(AppName, AppSecret, AppKey, callerId, Filter); 
-                
+                WebPhoneApi.HhaExchange2.CallerIDLookupResponse restResult = clientSoap.GetCallerDataByCallerID(AppName, AppSecret, AppKey, callerId, Filter);
+
                 //2. HHAExchange return a Coordinator1 Name
                 if (restResult != null && restResult.Result != null)
                 {
                     if (restResult.Result.Status.Equals(Infrastructure.StatusCode.Success))
                     {
-                        respone.Status = Infrastructure.StatusCode.Success; 
+                        respone.Status = Infrastructure.StatusCode.Success;
                         foreach (var item in restResult.LookupData)
                         {
                             if (!String.IsNullOrEmpty(item.Coordinator1))
                             {
                                 // get extension and coordinator
                                 //3. Query Extension Number of Coordinator in TeleGo DB 
-                                Extension data = new Extension();
-                                data.Data = customerCoordinateService.GetExtension(customerId, item.Coordinator1);  
-                                //add to list 
-                                respone.Extensions.Add(data);
+                                respone.Data = customerCoordinateService.GetExtension(customerId, item.Coordinator1);
+                                
                             }
                         }
                     }
                     else if (restResult.Result.Status.Equals(Infrastructure.StatusCode.Failure.ToString()))
                     {
-                        respone.Status = Infrastructure.StatusCode.Failure; 
-                        respone.ErrorInfo.MessageInfo = restResult.Result.ErrorInfo.ErrorMessage;
-                    } 
-                } 
-                
+                        respone.Status = Infrastructure.StatusCode.Failure;
+                        respone.Message = restResult.Result.ErrorInfo.ErrorMessage;
+                    }
+                }
+
             }
             else
             {
-                respone.Status = Infrastructure.StatusCode.Failure; 
-                respone.ErrorInfo.MessageInfo = "Not enough information";
+                respone.Status = Infrastructure.StatusCode.Failure;
+                respone.Message = "Not enough information";
             }
             //4. Return Extension to PBX
             return respone;

75512cef891cde9d782db7a12272b563b76ff8d4
anhv <voanh85@gmail.com>
2014-08-23 00:57:51 +0700

    cross domain

--- WebPhoneApi/WebPhoneApi/Controllers/PhoneController.cs
+++ WebPhoneApi/WebPhoneApi/Controllers/PhoneController.cs
@@ -9,10 +9,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Xml.Linq;
 
 namespace TeleGoApi.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class PhoneController : ApiController
     {
         private string appStart = string.Empty;
